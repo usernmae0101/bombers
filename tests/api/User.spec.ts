5dc136f3d09cb876b0918956fda49e469e30ab6f
bombers0 <bombers03@mail.ru>
2021-05-05 18:52:57 +0700

    update: CI (run mongo background)

--- tests/api/User.spec.ts
+++ tests/api/User.spec.ts
@@ -1,3 +1,7 @@
-describe("should ", () => {
-    it("should", () => {});
+const describeif = condition => condition ? describe : describe.skip;
+
+describeif(process.env.CI)("should ", () => {
+    it("should", () => {
+        console.log(1);
+    });
 });
